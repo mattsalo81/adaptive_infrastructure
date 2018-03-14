8dfd89d91b5a7218f2e370eee5fb045f46553d5d
Mathew Salo <a0227341@ti.com>
2018-03-14 16:42:12 -0500

    Built logic for updating the process code table built tests

--- packages/ProcessOptions/ProcessEncoder.pm
+++ packages/ProcessOptions/ProcessEncoder.pm
@@ -6,24 +6,74 @@ use Carp;
 use Data::Dumper;
 use Database::Connect;
 use ProcessOptions::ProcessDecoder;
-use LOGGING;
+use Logging;
 
-my $delete_code_sth;
-my $update_option_sth;
 my $table = "process_code_to_option";
 
-sub update_options{
-	my ($trans, $tech, $code_num, $code, $options) = @_;
-	unless (defined $update_option_sth){
-		# need to somehow do this uniquely by transaction... one static sth will be very bad
-		my $sql = qq{
-			insert into $table 
-				(TECHNOLOGY, CODE_NUM, PROCESS_CODE, PROCESS_OPTION)
-			values
-				(?, ?, ?, ?)
-		};
+
+# takes technology, code_number, and a lookup table (hashref)
+# keys of lookup table are codes, values are array-refs of options
+# ie :
+# $lookup = {
+#		'AB' 	-> 	['OPT1', 'OPT2'],
+#		'BC' 	-> 	['OPT2', 'OPT3'],
+# }
+sub update_code{
+	my ($tech, $code_num, $lookup) = @_;
+	my $trans = Connect::new_transaction("etest");
+	Logging::event("Updating ($tech - code $code_num) in database");
+	eval{
+		my $del_sth = get_delete_code_in_trans_sth($trans);
+		unless($del_sth->execute($tech, $code_num)){
+			confess "Could not empty codes ($tech - $code_num) in database";
+		}
+		my $u_sth = get_update_options_in_trans_sth($trans);
+		foreach my $code (keys %{$lookup}){
+			Logging::debug("Updating ($tech - $code_num - $code) in database");
+			foreach my $option (@{$lookup->{$code}}){
+				Logging::diag("Updating ($tech - $code_num - $code -> $option) in database");
+				unless($u_sth->execute($tech, $code_num, $code, $option)){
+					confess "Could not update ($tech - $code_num - $code -> $option) in database";
+				}
+			}
+		}
+		$trans->commit();
+		1;
+	} or do{
+		my $e = $@;
+		$trans->rollback();
+		confess "Could not update codes ($tech - $code_num) in database because of : $e";
+	};
+}
+
+sub get_update_options_in_trans_sth{
+	my ($trans) = @_;
+	my $sql = qq{
+		insert into $table 
+			(TECHNOLOGY, CODE_NUM, PROCESS_CODE, PROCESS_OPTION)
+		values
+			(?, ?, ?, ?)
+	};
+	my $sth = $trans->prepare($sql);
+	unless (defined $sth){
+		confess "Could not prepare get_update_options_in_trans_sth";
 	}
-	
+	return $sth;
+}
+
+sub get_delete_code_in_trans_sth{
+	my ($trans) = @_;
+	my $sql = qq{
+		delete from $table 
+		where 
+			technology = ?
+			and code_num = ?
+	};
+	my $sth = $trans->prepare($sql);
+        unless (defined $sth){
+                confess "Could not prepare delete_code_in_trans_sth";
+        }
+        return $sth;
 }
 
 1;
---
+++ packages/ProcessOptions/ProcessEncoder.t
@@ -0,0 +1,52 @@
+use warnings;
+use strict;
+use Test::More 'no_plan';
+use lib '/dm5/ki/adaptive_infrastructure/packages';
+require Test::Homebrew_Exception;
+use DBI;
+use ProcessOptions::ProcessEncoder;
+use ProcessOptions::ProcessDecoder;
+
+my @random_words = (
+	'THANG',
+	'THING',
+	'THINK',
+	'THANK',
+	'HUNK',
+	'HONK',
+	'HANK',
+	'HAND',
+	'HANG',
+);
+
+my $word1 = $random_words[int(rand(scalar @random_words - 1))];
+my $word2 = $random_words[int(rand(scalar @random_words - 1))];
+my $word3 = $random_words[int(rand(scalar @random_words - 1))];
+
+my $array1 = [$word1];
+my $array2 = [$word2];
+
+if (rand(1) > .5){
+	push @{$array1}, $word3;
+}else{
+	push @{$array2}, $word3;
+}
+
+my $lookup = {
+	CODE1	=>	$array1,
+	CODE2	=>	$array2,
+};
+
+ProcessEncoder::update_code("TEST2", 0, $lookup);
+ok(1, "didn't die when updating!");
+
+my @match1 = sort @{ProcessDecoder::get_options_for_code("TEST2", 0, "CODE1")};
+is(scalar @match1, scalar @{$array1} , "Found correct number of entries");
+my $str1 = join(", ", sort @{$array1});
+is(join(", ", sort @match1), $str1, "Found correct matches");
+
+my @match2 = sort @{ProcessDecoder::get_options_for_code("TEST2", 0, "CODE2")};
+is(scalar @match2, scalar @{$array2}, "Found correct number of entries");
+my $str2 = join(", ", sort @{$array2});
+is(join(", ", sort @match2), $str2, "Found correct matches");
+
