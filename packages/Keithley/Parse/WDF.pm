49456e8fe9f14d10d2e8de3733867bbb11a0178e
Mathew Salo <a0227341@ti.com>
2018-08-22 19:48:09 -0500

    Added WDF parser/creator

---
+++ packages/Keithley/Parse/WDF.pm
@@ -0,0 +1,168 @@
+package Parse::WDF;
+use warnings;
+use strict;
+use lib '/dm5/ki/adaptive_infrastructure/packages';
+use Carp;
+use Data::Dumper;
+use Logging;
+
+my $eoh = '<EOH>';
+my $eosites = '<EOSITES>';
+my $eosubsites = '<EOSUBSITES>';
+my $dummy_coord = 0.005;
+
+sub new{
+    my ($class, $text) = @_;
+    my $self = {
+        TEXT            => $text,
+        header          => [],
+        # subsite stuff
+        alignment_module=> undef,
+        modules         => {}, # 'my_subsite' -> [x_coord, y_coord]
+        dummy_modules   => {}, # 'dummy_sub' -> some_defined_val
+        # site stuff
+        orig_patterns   => {}, # 'LBC5_9site' -> { 1 => [x1, y1]}
+        sites           => {}, # {1 -> [x1, y1]}
+        patterns        => {},
+    };
+    bless $self, $class;
+    $self->parse();
+    return $self;
+} 
+
+sub get_new_text{
+    my ($self) = @_;
+    my $text = join("\n", @{$self->{'header'}});
+    $text .= "\n";
+    # unpack the patterns
+    foreach my $pattern (@{$self->get_pattern_order()}){
+        $text .= "Pattern,$pattern\n";        
+        foreach my $site (sort keys %{$self->{"patterns"}->{$pattern}}){
+            my ($x, $y) = @{$self->{"patterns"}->{$pattern}->{$site}};
+            $text .= "$site,$x,$y\n";
+        }
+    }
+    $text .= "$eosites\n";
+    $text .= "Site,Single,Single\n";
+    # unpack the subsites
+    my $a_mod = $self->{"alignment_module"};
+    confess "No alignment module found" unless defined $a_mod;
+    # alignment module
+    $text .= "$a_mod,0,0\n";
+    # alphabetical standard modules
+    foreach my $mod (sort keys %{$self->{"modules"}}){
+        my ($x, $y) = @{$self->{"modules"}->{$mod}};
+        $text .= "$mod,$x,$y\n";
+    }
+    # dummy
+    foreach my $dummy (sort keys %{$self->{"dummy_modules"}}){
+        $text .= "$dummy,$dummy_coord,$dummy_coord\n";
+    }
+    $text .= "$eosubsites";
+    return $text;
+}
+
+# order by lowest site number used
+sub get_pattern_order{
+    my ($self) = @_;
+    my $patterns = $self->{"patterns"};
+    my %pattern_min_site;
+    foreach my $pattern (keys %{$patterns}){
+        my $min;
+        # get lowest site number for this pattern
+        foreach my $site (keys %{$patterns->{$pattern}}){
+            if ((not defined $min) || $site < $min){
+                $min = $site;
+            }
+        }
+        confess "Found pattern with no sites!" unless defined $min;
+        $pattern_min_site{$pattern} = $min;
+    }
+    my @sorted = sort {$pattern_min_site{$a} <=> $pattern_min_site{$b}} keys %pattern_min_site;
+    return \@sorted;
+}
+
+sub parse{
+    my ($self) = @_;
+    my @lines = split(/\n/, $self->{"TEXT"});
+    # remove leading/trailing whitespace and delete blank lines
+    @lines = map {s/^\s*//; $_} @lines;
+    @lines = map {s/\s*$//; $_} @lines;
+    @lines = grep {m/\S/} @lines;
+    my $location = "header";
+    my $pattern;
+    foreach my $line (@lines){
+        if($location eq "header"){      # PARSE HEADER
+            push @{$self->{"header"}}, $line;
+            $location = "sites" if ($line eq $eoh);
+        }elsif($location eq "sites"){   # PARSE SITES
+            # update pattern
+            if($line =~ m/^Pattern,(\S*)$/){
+                $pattern = $1;
+            }elsif($line =~ m/^([0-9]+),([0-9]+),([0-9]+)$/){
+                confess "Undefined pattern at <$line>" unless defined $pattern;
+                $self->add_shot($pattern, $1, $2, $3);
+            }elsif($line eq $eosites){
+                $location = "subsites";
+            }else{
+                confess "Unexpecte WDF line format <$line>";
+            }
+        }elsif($location eq "subsites"){# PARSE SUBSITES
+            if($line =~ m/^site,/i){
+                # do nothing
+            }elsif($line =~ m/^[a-z0-9][a-z0-9_]*,[^,]+,[^,]+$/i){
+                my ($mod, $x, $y) = split(/,/, $line);
+                $self->add_mod($mod, $x, $y);
+            }elsif($line eq $eosubsites){
+                $location = "done";
+            }
+        }elsif($location eq "done"){    # HOLD
+            # do nothing
+        }else{
+            confess "Unexpected parser state <$location> in <$line>";
+        }
+    }
+    confess "Parser did not complete correctly! ended in state <$location>" unless $location eq "done";
+    $self->{"patterns"} = $self->{"orig_patterns"}; 
+}
+
+sub add_mod{
+    my ($self, $mod, $x, $y) = @_;
+    if (is_alignment($x, $y)){
+        $self->{"alignment_module"} = $mod;
+    }elsif (is_dummy($x, $y)){
+        $self->add_dummy($mod);   
+    }else{
+        $self->{"modules"}->{$mod} = [$x, $y];
+    }
+}
+
+sub add_dummy{
+    my ($self, $mod) = @_;
+    $self->{"dummy_modules"}->{$mod} = 1;
+}
+
+sub is_dummy{
+    my ($x, $y) = @_;
+    return (($x == $dummy_coord) && ($y == $dummy_coord));
+}
+
+sub is_alignment{
+    my ($x, $y) = @_;
+    return (($x == 0) && ($y == 0));
+}
+
+sub add_shot{
+    my ($self, $pattern, $site, $x, $y) = @_;
+    $self->{"orig_patterns"}->{$pattern}->{$site} = [$x, $y];
+    my $orig = $self->{"sites"}->{$site};
+    if(defined $orig){
+        my ($ox, $oy) = ($orig->[0], $orig->[1]);
+        if (($ox != $x) || ($oy != $y)){
+            confess "Site <$site> has conflicting coordinates, originally [$ox, $oy], found [$x, $y]";
+        }
+    }
+    $self->{"sites"}->{$site} = [$x, $y];    
+}
+
+1;
---
+++ packages/Keithley/Parse/WDF.t
@@ -0,0 +1,61 @@
+use warnings;
+use strict;
+use Test::More 'no_plan';
+use lib '/dm5/ki/adaptive_infrastructure/packages';
+require Test::Homebrew_Exception;
+require Test::Lists;
+use Keithley::Parse::WDF;
+use Data::Dumper;
+
+my $wdf_text = get_text();
+
+my $wdf = Parse::WDF->new($wdf_text);
+$wdf->{"TEXT"} = "";
+my $new_text = $wdf->get_new_text();
+is($new_text, $wdf_text, "WDF parsed/loaded/printed without modification and without aid of original text");
+
+sub get_text{
+    return q{#Keithley Wafer Description File
+Version,1.1
+File,M06CDC65310C0.wdf
+Date,06/08/2017
+Comment,Generated For Adaptive Test
+Project,Single
+DiameterUnits,Metric
+Diameter,200.0000
+Units,Metric
+DieSizeX,20.320
+DieSizeY,21.150
+Orientation,Notch,Left
+WaferOffset,0,0
+Axis,1
+Origin,5,5
+Target,5,5
+AutoAlignLocation,0.0000,0.0000
+Optimize,0
+<EOH>
+Pattern,STD
+1,7,7
+Pattern,WAS
+2,7,3
+3,3,4
+4,3,7
+5,5,5
+Pattern,REL
+6,5,8
+7,8,5
+8,5,2
+9,2,5
+Pattern,STD_ALT
+10,5,1
+11,7,2
+<EOSITES>
+Site,Single,Single
+lbc5_scm_mod00,0,0
+lbc5_scm_esd01,3.679,-4.230
+lbc5_scm_exmod2,5.144,0.000
+lbc5_scm_wlr05_v,0.005,0.005
+lbc5_wlr2v,0.005,0.005
+lbc5_wlr_mod02,0.005,0.005
+<EOSUBSITES>};
+}
